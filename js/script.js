349e7083c62b54cb7532e58404582a2de539aabe
Darren Wu <pochen@utexas.edu>
2017-09-03 03:22:49 -0500

    Added support for touch drag.

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ var drag = function() {
         divid.style.left = xpos + 'px';
         divid.style.top = ypos + 'px';
     },
-    startMoving : function(divid, container, evt) {
-      evt = evt || window.event;
-      var posX = evt.clientX,
-          posY = evt.clientY,
+    // For non-touch devices
+    onmousedown : function(divid, container, e) {
+      e = e || window.event;
+      var posX = e.clientX,
+          posY = e.clientY,
       divTop = divid.style.top,
       divLeft = divid.style.left,
       eWi = parseInt(divid.style.width),
@@ -19,10 +20,10 @@ var drag = function() {
       divLeft = divLeft.replace('px','');
       var diffX = posX - divLeft,
           diffY = posY - divTop;
-      document.onmousemove = function(evt) {
-        evt = evt || window.event;
-        var posX = evt.clientX,
-            posY = evt.clientY,
+      document.onmousemove = function(e) {
+        e = e || window.event;
+        var posX = e.clientX,
+            posY = e.clientY,
             aX = posX - diffX,
             aY = posY - diffY;
             // if (aX < 0) aX = 0;
@@ -32,10 +33,43 @@ var drag = function() {
         drag.move(divid,aX,aY);
       }
     },
-    stopMoving : function(container) {
+    onmouseup : function(container) {
       var a = document.createElement('script');
       document.getElementById(container).style.cursor='default';
       document.onmousemove = function() {}
+    },
+
+    // For touch devices
+    ontouchstart : function(divid, container, e) {
+      e = e || window.event
+      var touch = e.changedTouches[0];
+      var posX = touch.clientX,
+          posY = touch.clientY,
+      divTop = divid.style.top,
+      divLeft = divid.style.left,
+      eWi = parseInt(divid.style.width),
+      eHe = parseInt(divid.style.height),
+      cWi = parseInt(document.getElementById(container).style.width),
+      cHe = parseInt(document.getElementById(container).style.height);
+      document.getElementById(container).style.cursor='move';
+      divTop = divTop.replace('px','');
+      divLeft = divLeft.replace('px','');
+      var diffX = posX - divLeft,
+          diffY = posY - divTop;
+      document.ontouchmove = function(e) {
+        e = e || window.event;
+        var touch = e.changedTouches[0];
+        var posX = touch.clientX,
+            posY = touch.clientY,
+            aX = posX - diffX,
+            aY = posY - diffY;
+        drag.move(divid,aX,aY);
+      }
+    },
+    ontouchend : function(container) {
+      var a = document.createElement('script');
+      document.getElementById(container).style.cursor='default';
+      document.ontouchend = function() {}
     },
   }
 } ();
