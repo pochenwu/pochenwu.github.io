78686b326b10a5217c0a9e07da2e318982198927
Darren Wu <pochen@utexas.edu>
2017-09-04 15:41:23 -0500

    Refined animation with Observer as event lisetner.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // Components
 Vue.component('profile-card', {
   props: ['card'],
+  data: {
+    clicked: false
+  },
   template: `
     <div class="profile-card-wrapper">
       <div :style="{ backgroundImage: 'url(' + card.imgSrc + ')' }"
@@ -20,7 +23,8 @@ Vue.component('profile', {
   template: `
     <div id="profile">
       <profile-card v-if="cardIndex + 1 < cards.length" :card="cards[cardIndex + 1]" id="next-card"></profile-card>
-      <profile-card :card="cards[cardIndex]" id="current-card"></profile-card>
+      <profile-card v-if="cardIndex < cards.length" :card="cards[cardIndex]" id="current-card"></profile-card>
+      <div v-if="cardIndex === cards.length">There is no more card. It's a match? ;)</div>
     </div>
   `
 })
@@ -45,8 +49,10 @@ window.addEventListener('load', function() {
     }
   })
 
-  // Touch Control
+  // Touch and Animation Control
   var currentCard = document.getElementById('current-card');
+  var nextCard = document.getElementById('next-card');
+
   var currentCardTM = new Hammer.Manager(currentCard, {recognizers: [
       // RecognizerClass, [options], [recognizeWith, ...], [requireFailure, ...]
       [ Hammer.Tap ],
@@ -64,12 +70,15 @@ window.addEventListener('load', function() {
     // Convert to radians and use the Sine function to neutralize
     // negative degrees as well as manage magnitude
     var angle = Math.sin((e.angle - 90) * Math.PI / 180) * 2;
+    currentCard.style.transition = "rotate 200ms ease-in";
     currentCard.style.transform = "rotate(" + angle + "deg) translate(" + e.deltaX + "px," + e.deltaY + "px)";
   });
 
   currentCardTM.on("panend", function(e) {
     var angle = Math.sin((e.angle - 90) * Math.PI / 180) * 2;
-    var swipeOutAnimation = anime({
+    // TODO: use edge coordinates instead of arbitrary values.
+    // TODO: change duration based on velocity.
+    var swipeOutAnime = anime({
       targets: currentCard,
       translateX: [
         { value: e.deltaX, duration: 0, delay: 0, elasticity: 0 },
@@ -91,18 +100,18 @@ window.addEventListener('load', function() {
       }
     });
 
-    var resetAnimation = anime({
+    var resetPositionAnime = anime({
       targets: currentCard,
       translateX: [
         { value: e.deltaX, duration: 0, delay: 0, elasticity: 0 },
-        { value: 0, duration: 500, delay: 0, elasticity: 0 }
+        { value: 0, duration: 300, delay: 0, elasticity: 0 }
       ],
       translateY: [
         { value: e.deltaY, duration: 0, delay: 0, elasticity: 0 },
-        { value: 0, duration: 500, delay: 0, elasticity: 0 }
+        { value: 0, duration: 300, delay: 0, elasticity: 0 }
       ],
       rotate: { value: 0, duration: 5 },
-      duration: 500,
+      duration: 300,
       elasticity: 10,
       autoplay: false
     });
@@ -110,21 +119,59 @@ window.addEventListener('load', function() {
     console.log('velocity', e.velocity)
     console.log('destance', e.distance)
     console.log('angle', angle)
-    if ((e.velocity > 1.75 && e.distance > 100) || (angle < 0 && e.distance > 250)) {
+    if ((e.velocity > 1 && e.distance > 100) || (angle < 0 && e.distance > 250)) {
       console.log('Swipe Right');
-      swipeOutAnimation.play();
-    } else if ((e.velocity < -1.75 && e.distance > 100) || (angle > 0 && e.distance > 250)) {
+      swipeOutAnime.play();
+    } else if ((e.velocity < -1 && e.distance > 100) || (angle > 0 && e.distance > 250)) {
       console.log('Swipe Left');
-      swipeOutAnimation.play();
+      swipeOutAnime.play();
     } else {
-      resetAnimation.play();
+      resetPositionAnime.play();
     }
   })
+
+  // Preview Animation Event Listener
+  // TODO: make the evengt specific
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutationRecord) {
+      var transformValue = mutationRecord.target.style.transform
+      var translateX = getComputedTranslate('X', mutationRecord.target);
+      var translateY = getComputedTranslate('Y', mutationRecord.target);
+      var dist = Math.sqrt( translateX * translateX + translateY * translateY);
+      previewAnimation(nextCard.children[0], dist)
+    });
+  });
+
+  observer.observe(currentCard, { attributes : true, attributeFilter : ['style'] });
 })
 
-var swipeOutAnimation = function(angle, x, y, target) {
-  // target.style.transition = "all 500ms ease-out";
-  // target.style.transform = "angle(" + angle + "deg)";
-  // // TODO: use the edges instead of arbitrary cordinates
-  // target.style.transform += "translate(" + x * 5 + "px," + y * 5 + "px)";
+
+var previewAnimation = function(target, dist, reverse) {
+  // Preview animation of the next card
+  var dynamicSize = Math.max('.975', Math.min(1, .975 + dist / 1000));
+  target.style.transform = "scale(" + dynamicSize + ")"
+}
+
+function getComputedTranslate(coordinate, obj) {
+  // Credit: https://stackoverflow.com/questions/21912684/how-to-get-value-of-translatex-and-translatey
+  var index3D, index;
+  switch (coordinate) {
+    case 'X':
+      index3D = 12;
+      index = 4;
+      break;
+    case 'Y':
+      index3D = 13;
+      index = 5;
+    break;
+    default:
+      return 0;
+  }
+  if(!window.getComputedStyle) return;
+  var style = getComputedStyle(obj),
+      transform = style.transform || style.webkitTransform || style.mozTransform;
+  var mat = transform.match(/^matrix3d\((.+)\)$/);
+  if(mat) return parseFloat(mat[1].split(', ')[index3D]);
+  mat = transform.match(/^matrix\((.+)\)$/);
+  return mat ? parseFloat(mat[1].split(', ')[index]) : 0;
 }
