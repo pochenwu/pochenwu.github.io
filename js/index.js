6792c4c3c68e58c0ec754c54f550cff8cd5099b8
Darren Wu <pochen@utexas.edu>
2017-09-04 13:34:04 -0500

    Added better animation.

--- js/index.js
+++ js/index.js
@@ -64,37 +64,67 @@ window.addEventListener('load', function() {
     // Convert to radians and use the Sine function to neutralize
     // negative degrees as well as manage magnitude
     var angle = Math.sin((e.angle - 90) * Math.PI / 180) * 2;
-    currentCard.style.transitionDuration = '0ms';
-    currentCard.style.transform = "rotate(" + angle + "deg)";
-    currentCard.style.transform += "translate(" + e.deltaX + "px," + e.deltaY + "px)";
+    currentCard.style.transform = "rotate(" + angle + "deg) translate(" + e.deltaX + "px," + e.deltaY + "px)";
   });
 
   currentCardTM.on("panend", function(e) {
     var angle = Math.sin((e.angle - 90) * Math.PI / 180) * 2;
+    var swipeOutAnimation = anime({
+      targets: currentCard,
+      translateX: [
+        { value: e.deltaX, duration: 0, delay: 0, elasticity: 0 },
+        { value: e.deltaX * 5, duration: 500, delay: 0, elasticity: 0 }
+      ],
+      translateY: [
+        { value: e.deltaY, duration: 0, delay: 0, elasticity: 0 },
+        { value: e.deltaY * 5, duration: 500, delay: 0, elasticity: 0 }
+      ],
+      rotate: angle,
+      duration: 500,
+      elasticity: 0,
+      autoplay: false,
+      complete: function() {
+        app.showNextCard()
+        currentCard.style.transition = "translate 0ms linear"
+        currentCard.style.transform = "translate(0, 0)";
+        console.log('Swipe Out Complete')
+      }
+    });
+
+    var resetAnimation = anime({
+      targets: currentCard,
+      translateX: [
+        { value: e.deltaX, duration: 0, delay: 0, elasticity: 0 },
+        { value: 0, duration: 500, delay: 0, elasticity: 0 }
+      ],
+      translateY: [
+        { value: e.deltaY, duration: 0, delay: 0, elasticity: 0 },
+        { value: 0, duration: 500, delay: 0, elasticity: 0 }
+      ],
+      rotate: { value: 0, duration: 5 },
+      duration: 500,
+      elasticity: 10,
+      autoplay: false
+    });
+
     console.log('velocity', e.velocity)
     console.log('destance', e.distance)
     console.log('angle', angle)
-    if ((e.velocity > 1.75 && e.distance > 100) || (angle < 0 && e.distance > 200)) {
+    if ((e.velocity > 1.75 && e.distance > 100) || (angle < 0 && e.distance > 250)) {
       console.log('Swipe Right');
-      swipeOutAnimation(angle, e.deltaX, e.deltaY, currentCard);
-      app.showNextCard()
-      currentCard.style.transition = "all 0ms linear"
-      currentCard.style.transform = "translate(0, 0)";
-    } else if ((e.velocity < -1.75 && e.distance > 100) || (angle > 0 && e.distance > 200)) {
-      console.log('Swipe Left')
-      app.showNextCard()
-      currentCard.style.transition = "all 0ms linear"
-      currentCard.style.transform = "translate(0, 0)";
+      swipeOutAnimation.play();
+    } else if ((e.velocity < -1.75 && e.distance > 100) || (angle > 0 && e.distance > 250)) {
+      console.log('Swipe Left');
+      swipeOutAnimation.play();
     } else {
-      currentCard.style.transition = "all 350ms cubic-bezier(0.6, 0.2, 0.6, 1.6)";
-      currentCard.style.transform = "translate(0, 0)";
+      resetAnimation.play();
     }
   })
 })
 
 var swipeOutAnimation = function(angle, x, y, target) {
-  target.style.transition = "all 500ms ease-out";
-  target.style.transform = "angle(" + angle + "deg)";
-  // TODO: use the edges instead of arbitrary cordinates
-  target.style.transform += "translate(" + x * 5 + "px," + y * 5 + "px)";
+  // target.style.transition = "all 500ms ease-out";
+  // target.style.transform = "angle(" + angle + "deg)";
+  // // TODO: use the edges instead of arbitrary cordinates
+  // target.style.transform += "translate(" + x * 5 + "px," + y * 5 + "px)";
 }
