ff07b7abb17f19cd47a06c6164dfd01ba7d0bece
Darren Wu <pochen@utexas.edu>
2017-09-11 16:38:33 -0500

    Added local copy of fontawesome. Minor refactor for animation to be used on buttons. Added data points.

--- js/data.js
+++ js/data.js
@@ -2,25 +2,28 @@ var data =
 {
   "cards": [
     {
-      "collage": ["./assets/imgs/p1.jpg"],
+      "title": "University of Texas at Austin",
+      "collage": ["./assets/imgs/profile-large.jpg"],
       "name": "Po-Chen",
       "age": "23",
-      "title": "University of Texas at Austin",
       "position": "BA, Computer Sciense",
-      "gpa": "4.00",
       "class": "education",
       "date": "May 2018",
-      "message": ""
+      "message": "Senior CS major. Looking for 2018 internship/fulltime opportunities. Eligible to work in the U.S.; will require visa sponsorship for full-time employment.",
+      "bullets": [
+        "Overall GPA: 4.00",
+        "Additional Coursework in Business, 13 hours"
+      ]
     },
     {
+      "title": "WizeHire Inc",
       "collage": ["./assets/imgs/p2.jpg"],
       "name": "Po-Chen",
       "age": "23",
-      "title": "WizeHire Inc",
       "position": "Software Engineering Intern",
       "class": "work",
-      "message": "",
       "date": "May 2017 - Aug 2017",
+      "message": "",
       "bullets": [
         "Focused on full-stack development for service oriented web apps.",
         "Built an API-based infrastructure of push notification service using Socket.io.",
@@ -31,14 +34,86 @@ var data =
       ]
     },
     {
+      "title": "UT Austin Sanger Learning Center",
+      "collage": ["./assets/imgs/army.jpg"],
+      "name": "Po-Chen",
+      "age": "23",
+      "position": "Tutor",
+      "class": "work",
+      "date": "Jan 2016 - May 2016",
+      "message": "",
+      "bullets": [
+        "Employed by the School of Undergraduate Studies for student-on-student tutoring",
+        "Taught collegiate level STEM courses: Calculus I & II, and Intro to Programming (Java)"
+      ]
+    },
+    {
+      "title": "Taiwan Army Infantry School",
+      "collage": ["./assets/imgs/army.jpg"],
+      "name": "Po-Chen",
+      "age": "23",
+      "position": "2 CL PVT; Student Leader",
+      "class": "work",
+      "date": "May 2014 - Aug 2014",
+      "message": "",
+      "bullets": [
+        "Specialized in T-75 60mm Infantry Mortar",
+        "Coordinated daily programs and schedules in details for 27 members of 1B2C",
+        "Teaching assistant in mortar training sessions"
+      ]
+    },
+    {
+      "title": "Studnet Conduct Board",
       "collage": ["./assets/imgs/p3.jpg"],
       "name": "Po-Chen",
       "age": "23",
-      "title": "Studnet Conduct Board",
+      "position": "Board Member",
+      "class": "activity",
+      "date": "Fall 2017 - Current",
+      "message": "",
+      "bullets": [
+        'Appointed directly by the President of UT Austin for a two-year tenure',
+        'Conduct official hearings for students seeking a review on institutional disciplinary actions'
+      ]
+    },
+    {
+      "title": "Taiwanese Int'l Student Association",
+      "collage": ["./assets/imgs/tisa.jpg"],
+      "name": "Po-Chen",
+      "age": "23",
       "position": "Member",
       "class": "activity",
-      "message": "Hello World",
-      "date": "Fall 2017 - Current"
+      "date": "Fall 2015 - Current",
+      "message": "",
+      "bullets": [
+        'Designed club T-shirt for school year 2016',
+        'Raise awareness of diversity in the student body'
+      ]
+    },
+    {
+      "title": "Phi Betta Kappa",
+      "collage": ["./assets/imgs/honors.jpg"],
+      "name": "Po-Chen",
+      "age": "23",
+      "position": "Junior Member",
+      "class": "award",
+      "date": "Fall 2016 - Current",
+      "message": "",
+      "bullets": [
+        'Elected with prestigious Junior membership',
+        'Recognized for distinctive intellectual capacities in liberal arts and sciences'
+      ]
+    },
+    {
+      "title": "Distinguished College Scholar",
+      "collage": ["./assets/imgs/honors.jpg"],
+      "name": "Po-Chen",
+      "age": "23",
+      "position": "",
+      "class": "award",
+      "date": "Spring 2017",
+      "message": "Awarded for being top 4% of class.",
+      "bullets": []
     }
   ],
   links: {
--- js/index.js
+++ js/index.js
@@ -59,15 +59,17 @@ Vue.component('profile', {
     profileModalToggle: function () {
       return this.isCardOpen ? { marginLeft : 0, marginRight: 0, top: 0, width: '100%', height: '100%' } : {}
     }
+  },
+  methods: {
   }
 })
 
 Vue.component('bot-nav', {
-  props: ['links'],
+  props: ['links', 'participants', 'closeCard', 'showNextCard', 'showMatchNotice'],
   template: `
     <div id="bot-nav">
       <button class="button button-circle button-large bot-nav-item" style="color: #11B1F1" @click="open(links.linkedin)"><i class="fa fa-linkedin"></i></button>
-      <button class="button button-circle button-jumbo bot-nav-item" style="color: #FF5268" @click="nope"><i class="fa fa-times"></i></button>
+      <button class="button button-circle button-jumbo bot-nav-item" style="color: #FF5268" @click="next"><i class="fa fa-times"></i></button>
       <button class="button button-circle button-large bot-nav-item" style="color: #FFBA03" @click="undo"><i class="fa fa-undo"></i></button>
       <button class="button button-circle button-jumbo bot-nav-item" style="color: #41E9C1" @click="like"><i class="fa fa-heart"></i></button>
       <button class="button button-circle button-large bot-nav-item" style="color: #AA51E4" @click="open(links.github)"><i class="fa fa-git"></i></button>
@@ -77,37 +79,50 @@ Vue.component('bot-nav', {
     open: function (url) {
       window.open(url)
     },
-    nope: function () {
-      console.log('nope')
+    next: function () {
+      this.closeCard()
+      swipeOutAnime(this.participants, -85, 0, 5, () => {
+        this.showNextCard()
+      });
     },
     undo: function () {
       console.log('undo')
     },
     like: function () {
+      this.closeCard()
+      // TODO: delta x needs to commodate responsive sizes
+      swipeOutAnime(this.participants, 85, 0, 5, () => {
+        this.showNextCard()
+        this.showMatchNotice()
+      });
       console.log('like')
     }
   }
 })
 
 Vue.component('match-notice', {
-  props: ['isMatch', 'resetApp', 'sendMessage'],
+  props: ['profileImgSrc', 'isMatch', 'resetApp', 'sendMessage', 'closeMatchNotice'],
   template: `
     <div id="match-notice" v-bind:class="{ 'hide-match': !isMatch }">
       <h1>It's a Match?</h1>
       <p>You seemd to have liked Po-Chen.</p>
       <div class="match-avatar">
-        <div class="avatar"></div>
+        <div class="avatar" :style="{ backgroundImage: 'url(' + profileImgSrc + ')' }"></div>
       </div>
       <div class="match-button-group">
         <button class="my-button" @click="sendMessage">
           <i class="fa fa-comment" aria-hidden="true"></i>
           <span class="my-button-text">Send Message</span>
         </button>
-        <button class="my-button" @click="resetApp">
-          <i class="fa fa-refresh" aria-hidden="true"></i>
-          <span class="my-button-text">Play Again</span>
+        <button class="my-button" @click="closeMatchNotice">
+          <i class="fa fa-address-book-o" aria-hidden="true"></i>
+          <span class="my-button-text">Keep Playing</span>
         </button>
       </div>
+      <button class="my-button" @click="resetApp" style="width: 75%; border: none; margin: 5%; margin-top: auto;">
+        <i class="fa fa-repeat" aria-hidden="true"></i>
+        <span class="my-button-text">Replay</span>
+      </button>
     </div>
   `
 })
@@ -119,8 +134,13 @@ window.addEventListener('load', function() {
     el: '#app',
     template: `
       <div id="app">
-        <match-notice :isMatch="isMatch" :resetApp="resetApp" :sendMessage="sendMessage"></match-notice>
-        
+        <match-notice
+          :isMatch="isMatch"
+          :resetApp="resetApp"
+          :sendMessage="sendMessage"
+          :closeMatchNotice="closeMatchNotice"
+          :profileImgSrc="lastProfileImgSrc">
+        </match-notice>
         <div id="main-view"  :class="{ 'blur': isMatch }">
           <div id="top-nav">
             <button class="button button-plain button-borderless button-large top-nav-item" style="color: #DADFE6">
@@ -133,11 +153,13 @@ window.addEventListener('load', function() {
           </div>
           <div id="shadow-profile"></div>
           <profile :cards="cards" :card-index="cardIndex" :is-card-open="isCardOpen"></profile>
-          <bot-nav :links="links"></bot-nav>
+          <bot-nav :links="links" :participants="participants" :closeCard="closeCard" :showNextCard="showNextCard" :showMatchNotice="showMatchNotice"></bot-nav>
         </div>
       </div>
     `,
     data: {
+      participants: {},
+      lastProfileImgSrc: '',
       isCardOpen: isCardOpen,
       isMatch: false,
       cardIndex: 0,
@@ -146,6 +168,8 @@ window.addEventListener('load', function() {
     },
     methods: {
       showNextCard: function () {
+        // TODO check if target exists.
+        this.lastProfileImgSrc = this.cards[this.cardIndex].collage[0]
         this.cardIndex++;
         if (this.cardIndex === this.cards.length) {
           this.showMatchNotice()
@@ -166,6 +190,9 @@ window.addEventListener('load', function() {
         this.isMatch = true
         console.log('It\'s a match!')
       },
+      closeMatchNotice: function () {
+        this.isMatch = false
+      },
       resetApp: function () {
         isCardOpen = false;
         this.isCardOpen = isCardOpen;
@@ -175,19 +202,26 @@ window.addEventListener('load', function() {
       sendMessage: function () {
         window.location.href = 'mailto:' + this.links.email
       }
-    }
+    },
+    mounted: function () {
+      // Make sure all children are rendered before we try to get the referenes
+      this.$nextTick(function () {
+        // DOM references
+        var profile = document.getElementById('profile')
+        var currentCard = document.getElementById('current-card');
+        var nextCard = document.getElementById('next-card');
+        var profilePic = currentCard.children[0]
+        this.participants = { currentCard, nextCard, profilePic }
+        registerProfileAnimation(app, this.participants)
+      })
+    },
   })
-  registerProfileAnimation(app)
 })
 
-var registerProfileAnimation = function (app) {
-  // DOM references
-  var mainView = document.getElementById('main-view')
-  var profile = document.getElementById('profile')
-  var currentCard = document.getElementById('current-card');
-  var nextCard = document.getElementById('next-card');
-  var profilePic = currentCard.children[0]
-
+var registerProfileAnimation = function (app, participants) {
+  var profilePic = participants.profilePic
+  var currentCard = participants.currentCard
+  var nextCard = participants.nextCard
   // Touch and Animation Control
   var profilePicTM = new Hammer.Manager(profilePic, {recognizers: [
       // RecognizerClass, [options], [recognizeWith, ...], [requireFailure, ...]
@@ -201,6 +235,7 @@ var registerProfileAnimation = function (app) {
     // Snapshots
     var appDimentionSnapshot = app.$el.getClientRects()[0]
     var profileDimentionSnapshot = profile.getClientRects()[0]
+    var snapshots = { appDimentionSnapshot, profileDimentionSnapshot }
     // Animation
     // BUG: card open vraibable no changing.
     profilePic.addEventListener('transitionend', function(e) {
@@ -217,40 +252,17 @@ var registerProfileAnimation = function (app) {
         profilePic.style.transition = 'unset';
         profilePic.style.height = null;
         currentCard.style.boxShadow = null;
+        participants.nextCard.style.visibility = null;
         console.log('Zoom out callback completed.', profilePic.style.height)
       }
     })
-    var zoomIn = {
-      play: function () {
-        console.log('Zooming in.')
-        // Resets
-        profilePic.style.transition = null;
-        // Obejectives
-        profilePic.style.height = appDimentionSnapshot.width + 'px';
-        currentCard.style.borderRadius = 0;
-        currentCard.style.boxShadow = 'none';
-      }
-    }
-    var zoomOut = {
-      play: function () {
-        console.log('Zooming out.')
-        // Initial state
-        profilePic.style.height = appDimentionSnapshot.width + 'px';
-        profilePic.style.paddingTop = null;
-        // Resets
-        profilePic.style.transition = null;
-        // Obejectives
-        currentCard.style.borderRadius = null;
-        profilePic.style.height = appDimentionSnapshot.height * .79 + 'px';
-      }
-    }
 
     if (!app.isCardOpen) {
       app.openCard()
-      zoomIn.play()
+      zoomInAnime(participants, snapshots)
     } else {
       app.closeCard()
-      zoomOut.play()
+      zoomOutAnime(participants, snapshots)
     }
   });
 
@@ -260,13 +272,14 @@ var registerProfileAnimation = function (app) {
       // Convert to radians and use the Sine function to neutralize
       // negative degrees as well as manage magnitude
       var angle = Math.sin((e.angle - 90) * Math.PI / 180) * 2;
-      // currentCard.style.transition = "rotate 200ms ease-in";
+      // This is the rotate and follow animation. Using pure CSS ensure the
+      // most responsive user experience
       currentCard.style.transform = "rotate(" + angle + "deg) translate(" + e.deltaX + "px," + e.deltaY + "px)";
       currentCard.style.boxShadow = 'none';
 
       // To prevent last card showing
-      if (app.cardIndex + 1 < app.cards.length) {
-        nextCard.style.visibility = "visible";
+      if (!(app.cardIndex + 1 < app.cards.length)) {
+        nextCard.style.visibility = "hidden";
       }
     }
   });
@@ -274,65 +287,21 @@ var registerProfileAnimation = function (app) {
   profilePicTM.on("panend", function(e) {
     if (!app.isCardOpen) {
       var angle = Math.sin((e.angle - 90) * Math.PI / 180) * 2;
-      // TODO: use edge coordinates instead of arbitrary values.
-      // TODO: change duration based on velocity.
-      var swipeOutAnime = anime({
-        targets: currentCard,
-        translateX: [
-          { value: e.deltaX, duration: 0, delay: 0, elasticity: 0 },
-          { value: e.deltaX * 5, duration: 250, delay: 0, elasticity: 0 }
-        ],
-        translateY: [
-          { value: e.deltaY, duration: 0, delay: 0, elasticity: 0 },
-          { value: e.deltaY * 5, duration: 250, delay: 0, elasticity: 0 }
-        ],
-        rotate: angle,
-        duration: 250,
-        elasticity: 0,
-        autoplay: false,
-        complete: function() {
-          app.closeCard()
-          app.showNextCard()
-          // TODO make this a animition
-          profilePic.style.boxShadow = null;
-
-          currentCard.style.transform = null;
-          nextCard.style.visibility = null;
-          // console.log('Swipe Out Complete')
-        }
-      });
-
-      var resetPositionAnime = anime({
-        targets: currentCard,
-        translateX: [
-          { value: e.deltaX, duration: 0, delay: 0, elasticity: 0 },
-          { value: 0, duration: 300, delay: 0, elasticity: 0 }
-        ],
-        translateY: [
-          { value: e.deltaY, duration: 0, delay: 0, elasticity: 0 },
-          { value: 0, duration: 300, delay: 0, elasticity: 0 }
-        ],
-        rotate: { value: 0, duration: 5 },
-        duration: 300,
-        elasticity: 10,
-        autoplay: false,
-        complete: function() {
-          currentCard.style.boxShadow = null;
-          nextCard.style.visibility = null;
-        }
-      });
-
       // console.log('velocity', e.velocity)
       // console.log('destance', e.distance)
       // console.log('angle', angle)
       if ((e.velocity > 1 && e.distance > 100) || (angle < 0 && e.distance > 250)) {
         // console.log('Swipe Right');
-        swipeOutAnime.play();
+        swipeOutAnime(participants, e.deltaX, e.deltaY, angle, () => {
+          app.showNextCard()
+          app.showMatchNotice()
+        });
       } else if ((e.velocity < -1 && e.distance > 100) || (angle > 0 && e.distance > 250)) {
-        // console.log('Swipe Left');
-        swipeOutAnime.play();
+        swipeOutAnime(participants, e.deltaX, e.deltaY, angle, () => {
+          app.showNextCard()
+        });
       } else {
-        resetPositionAnime.play();
+        resetCardAnime(participants, e.deltaX, e.deltaY);
       }
     }
   })
@@ -352,6 +321,78 @@ var registerProfileAnimation = function (app) {
   observer.observe(currentCard, { attributes : true, attributeFilter : ['style'] });
 }
 
+// Animation
+var zoomInAnime = function (participants, snapshots) {
+    console.log('Zooming in.')
+    // Initial state
+    participants.nextCard.style.visibility = "hidden";
+    // Resets
+    participants.profilePic.style.transition = null;
+    // Obejectives
+    participants.profilePic.style.height = snapshots.appDimentionSnapshot.width + 'px';
+    participants.currentCard.style.borderRadius = 0;
+    participants.currentCard.style.boxShadow = 'none';
+}
+
+var zoomOutAnime = function (participants, snapshots) {
+    console.log('Zooming out.')
+    // Initial state
+    participants.profilePic.style.height = snapshots.appDimentionSnapshot.width + 'px';
+    participants.profilePic.style.paddingTop = null;
+    // Resets
+    participants.profilePic.style.transition = null;
+    // Obejectives
+    participants.currentCard.style.borderRadius = null;
+    participants.profilePic.style.height = snapshots.appDimentionSnapshot.height * .79 + 'px';
+}
+
+var swipeOutAnime = function (participants, deltaX, deltaY, angle, callback) {
+  // TODO: use edge coordinates instead of arbitrary values.
+  // TODO: change duration based on velocity.
+  anime({
+    targets: participants.currentCard,
+    translateX: [
+      { value: deltaX, duration: 0, delay: 0, elasticity: 0 },
+      { value: deltaX * 5, duration: 350, delay: 0, elasticity: 0 }
+    ],
+    translateY: [
+      { value: deltaY, duration: 0, delay: 0, elasticity: 0 },
+      { value: deltaY * 5, duration: 350, delay: 0, elasticity: 0 }
+    ],
+    rotate: angle,
+    duration: 350,
+    elasticity: 0,
+    // autoplay: false,
+    complete: function() {
+      // TODO make this a animition
+      participants.profilePic.style.boxShadow = null;
+      participants.currentCard.style.transform = null;
+      participants.nextCard.style.visibility = null;
+      callback()
+    }
+  })
+};
+
+var resetCardAnime = function (participants, deltaX, deltaY, callback) {
+  anime({
+    targets: participants.currentCard,
+    translateX: [
+      { value: deltaX, duration: 0, delay: 0, elasticity: 0 },
+      { value: 0, duration: 300, delay: 0, elasticity: 0 }
+    ],
+    translateY: [
+      { value: deltaY, duration: 0, delay: 0, elasticity: 0 },
+      { value: 0, duration: 300, delay: 0, elasticity: 0 }
+    ],
+    rotate: { value: 0, duration: 5 },
+    duration: 300,
+    elasticity: 10,
+    complete: function() {
+      participants.currentCard.style.boxShadow = null;
+      participants.nextCard.style.visibility = null;
+    }
+  })
+}
 
 var previewAnimation = function (target, dist, reverse) {
   // Preview animation of the next card
@@ -381,18 +422,4 @@ function getComputedTranslate(coordinate, obj) {
   if(mat) return parseFloat(mat[1].split(', ')[index3D]);
   mat = transform.match(/^matrix\((.+)\)$/);
   return mat ? parseFloat(mat[1].split(', ')[index]) : 0;
-}
-
-function arrayToList(array) {
-  var list = '';
-  if (array.length > 0) {
-    list = document.createElement('ul');
-    for (var i in array) {
-      var il = document.createElement('il')
-      var t = document.createTextNode(array[i]);
-      il.appendChild(t)
-      list.appendChild(il)
-    }
-  }
-  return list;
 }
